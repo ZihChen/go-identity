05be7513a9b85f88a151cf8647445c2b7e6d6c75
Winston Lin <winston.l@jvd.tw>
2026-03-11 15:07:19 +0800

    refactor(domain): remove asynq.Handler from QueueService port

--- internal/domain/ports/outbound/service/queue.go
+++ internal/domain/ports/outbound/service/queue.go
@@ -1,34 +1,14 @@
 package service
 
-import (
-	"context"
-
-	"github.com/hibiken/asynq"
-)
+import "context"
 
 // QueueService 隊列服務接口
 type QueueService interface {
-	// EnqueueMerchantSync 將商戶同步任務加入隊列
 	EnqueueMerchantSync(ctx context.Context, data []byte) error
-
-	// EnqueuePlayerSync 將玩家同步任務加入隊列
 	EnqueuePlayerSync(ctx context.Context, data []byte) error
-
-	// EnqueueManagerSync 將管理員同步任務加入隊列
 	EnqueueManagerSync(ctx context.Context, data []byte) error
-
-	// EnqueueLevelSync 將會員等級同步任務加入佇列
 	EnqueueLevelSync(ctx context.Context, data []byte) error
-
-	// EnqueueTagSync 將會員標籤同步任務加入佇列
 	EnqueueTagSync(ctx context.Context, data []byte) error
-
-	// EnqueueAgentSync 將代理同步任務加入隊列
 	EnqueueAgentSync(ctx context.Context, data []byte) error
-
-	// WrapHandlerWithTracing 包裝處理器以添加追蹤功能
-	WrapHandlerWithTracing(h asynq.Handler) asynq.Handler
-
-	// Close 關閉隊列服務並釋放資源
 	Close() error
 }
